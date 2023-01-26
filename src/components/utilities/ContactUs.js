1061762b5e58029c7c152f16e494417c7cb037b8
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-26 10:53:12 -0600

    working on form

--- src/components/utilities/ContactUs.js
+++ src/components/utilities/ContactUs.js
@@ -5,7 +5,21 @@ import React from "react";
 const ContactUs = () => {
     return(
         <div>
-            <p>Here is your Template.</p>
+            <form id="contact-form">
+                <label>Name: </label>
+                <input type="text" name="name" placeholder="Your name here"></input>
+                <br />
+                <br></br>
+                <label>Email: </label>
+                <input type="email" name="email" placeholder="Your email here"></input>
+                <br />
+                <br></br>
+                <label>Message: </label>
+                <textarea name="message" placeholder="Your message here"></textarea>
+                <br />
+                <br></br>
+                <input type="submit" value="Send" id="submit-button"></input>
+            </form>
         </div>
     )
 };
--- src/components/utilities/ErrorPage.js
+++ src/components/utilities/ErrorPage.js
@@ -3,9 +3,10 @@ import React from "react";
 
 // Functional Component
 const ErrorPage = () => {
-    return(
-        <div>
-            <p>Here is your Template.</p>
+    return (
+        <div className="error-page">
+            <img className="error-image" src="https://freesvg.org/img/car-crash-clipart-publicdomain.png" alt="Error"/>
+            <h1 className="error-message">We apologize for the inconvenience, please try again later or contact our customer service team for assistance.</h1>
         </div>
     )
 };
