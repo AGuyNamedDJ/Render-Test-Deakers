7719ffdbec57f5df2a24adbf752af4f21bbbe766
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-27 11:55:50 -0600

    update

--- src/components/utilities/ContactUs.js
+++ src/components/utilities/ContactUs.js
@@ -9,20 +9,21 @@ const ContactUs = () => {
             <p className="contact-text">At Car Haven, we value your feedback and opinions. By filling out this contact form, you will be able to reach our customer service team and share any questions, concerns, or suggestions you may have. Whether you're inquiring about a specific vehicle or just want to give us your thoughts on your last visit, we're here to listen. Your input helps us improve our dealership and provide the best service possible. Thank you for choosing Car Haven.</p>
 
             <form id="contact-form">
-                <label>Name: </label>
-                <input type="text" name="name" placeholder="Your name here"></input>
-                <br />
-                <br></br>
-                <label>Email: </label>
-                <input type="email" name="email" placeholder="Your email here"></input>
-                <br />
-                <br></br>
-                <label>Message: </label>
-                <textarea name="message" placeholder="Your message here"></textarea>
-                <br />
-                <br></br>
-                <br></br>
-                <input type="submit" value="Send" id="submit-button"></input>
+                <div>
+                    <label>Name: </label>
+                    <input type="text" name="name" placeholder="Your name here" class="input-text"></input>
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input type="email" name="email" placeholder="Your email here" class="input-text"></input>
+                </div>
+                <div>
+                    <label>Message: </label>
+                    <textarea name="message" placeholder="Your message here" class="input-text-message"></textarea>
+                </div>
+                <div>
+                    <input type="submit" value="Send" id="submit-button" class="input-text"></input>
+                </div>
             </form>
         </div>
     )
